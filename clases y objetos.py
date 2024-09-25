d69cb21aafbc4d75b14dad62003f74713f8c8b5a
Yuri Urzua Lebuy <yuri19762008@hotmail.com>
2024-09-25 12:48:38 -0300

    clases y objetos

--- clases y objetos.py
+++ clases y objetos.py
@@ -3,9 +3,19 @@ class Celular:
         self.marca = marca
         self.modelo = modelo
         self.camara = camara
+        
+    #metodos
+    def llamar(self):
+        print(f'Esta haciendo un llamado desde un {self.modelo}')
+    
+    def cortar(self):
+        print(f'Cortaste la llamada desde tu: {self.modelo}')
+    
+    
 
 celular1 = Celular("Samsung","S23","48MP")
 celular2 = Celular("Apple","Iphone 15 pro","96MP")
 
 
-print(celular2.modelo)
+celular1.llamar()
+celular2.cortar()

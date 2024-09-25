c7f3fb228ddc5aa51045061fca5bf968035d6a24
Yuri Urzua Lebuy <yuri19762008@hotmail.com>
2024-09-25 12:34:52 -0300

    clase

--- clases y objetos.py
+++ clases y objetos.py
@@ -1,1 +1,11 @@
-"
+class Celular:
+    def __init__(self, marca, modelo, camara):
+        self.marca = marca
+        self.modelo = modelo
+        self.camara = camara
+
+celular1 = Celular("Samsung","S23","48MP")
+celular2 = Celular("Apple","Iphone 15 pro","96MP")
+
+
+print(celular2.modelo)

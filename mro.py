f201b057b864a1a65a831e2bc0cc7c2c9a6ab14f
Yuri Urzua Lebuy <yuri19762008@hotmail.com>
2024-09-27 16:42:18 -0300

    MRO

--- mro.py
+++ mro.py
@@ -1,2 +1,32 @@
-# 1:12:45 / 4:14:12
-#continuar video 
+class A:
+    def hablar (self):
+        print("hola desde A")
+class F(A):
+    def hablar (self):
+        print("hola desde F")
+        
+class B(A):
+    def hablar (self):
+        print("hola desde B")
+        
+class C(F):
+    def hablar (self):
+        print("hola desde C")
+        
+class D(B,C):
+    #def hablar (self):
+        #print("hola desde D")
+    pass
+
+d = D()
+
+print(D.mro())
+
+d.hablar()
+
+#especificar desde donde llamara la clase
+A.hablar(d)
+B.hablar(d)
+C.hablar(d)
+D.hablar(d)
+F.hablar(d)
